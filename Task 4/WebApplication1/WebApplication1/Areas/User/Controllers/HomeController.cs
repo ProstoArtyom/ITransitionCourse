b757a08b962edd6a2f94fb1045b8a8f8a6550df4
Артём Шашок <artyom.shashok@mail.ru>
2024-02-07 15:51:03 +0300

    Added user management view

--- Task 4/WebApplication1/WebApplication1/Areas/User/Controllers/HomeController.cs
+++ Task 4/WebApplication1/WebApplication1/Areas/User/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using WebApplication1.DataAccess.Repository.IRepository;
 using WebApplication1.Models;
 
 namespace WebApplication1.Areas.User.Controllers
@@ -10,15 +11,19 @@ namespace WebApplication1.Areas.User.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var applicationUsers = _unitOfWork.ApplicationUser.GetAll();
+            return View(applicationUsers);
         }
 
         public IActionResult Privacy()
