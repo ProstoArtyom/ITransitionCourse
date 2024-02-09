e2454043f53f28478e2754d103b4a37d8c5715c1
Артём Шашок <artyom.shashok@mail.ru>
2024-02-10 01:42:53 +0300

    Added model project and user

---
+++ Task 5/WebApplication1/WebApplication1.Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models;
+
+public class User
+{
+    public string Id { get; set; }
+    public int Number { get; set; }
+    public string Name { get; set; }
+    public string StreetAddress { get; set; }
+    public string PhoneNumber { get; set; }
+}
